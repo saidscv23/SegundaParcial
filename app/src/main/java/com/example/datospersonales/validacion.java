633d79c61ca07e6e2f66c34d73be83fe2a59f365
saidscv23 <said.carrillo438@ist17dejulio.edu.ec>
2024-02-14 09:33:17 -0500

    first commit

---
+++ app/src/main/java/com/example/datospersonales/validacion.java
@@ -0,0 +1,14 @@
+package com.example.datospersonales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class validacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validacion);
+    }
+}
