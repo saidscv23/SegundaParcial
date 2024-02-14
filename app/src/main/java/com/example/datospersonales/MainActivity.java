d22f3386e4a3776c51e4bcccaf3ce3ecea8309e8
saidscv23 <said.carrillo438@ist17dejulio.edu.ec>
2024-02-14 09:57:08 -0500

    Enviando datos y Recibiendo correctamente

--- app/src/main/java/com/example/datospersonales/MainActivity.java
+++ app/src/main/java/com/example/datospersonales/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.datospersonales;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txt_nombre, txt_telefono, txt_carrrera, txt_deporte, txt_edad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_nombre = findViewById(R.id.txt_nombre);
+        txt_telefono = findViewById(R.id.txt_telefono);
+        txt_carrrera = findViewById(R.id.txt_carrrera);
+        txt_deporte = findViewById(R.id.txt_deporte);
+        txt_edad = findViewById(R.id.txt_edad);
+
+        Button btn_ingresar = findViewById(R.id.btn_ingresar);
+        btn_ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nombre = txt_nombre.getText().toString();
+                String telefono = txt_telefono.getText().toString();
+                String carrera = txt_carrrera.getText().toString();
+                String deporte = txt_deporte.getText().toString();
+                String edad = txt_edad.getText().toString();
+
+
+                Intent intent = new Intent(MainActivity.this, validacion.class);
+                intent.putExtra("nombre", nombre);
+                intent.putExtra("telefono", telefono);
+                intent.putExtra("carrera", carrera);
+                intent.putExtra("deporte", deporte);
+                intent.putExtra("edad", edad);
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/datospersonales/validacion.java
+++ app/src/main/java/com/example/datospersonales/validacion.java
@@ -1,8 +1,9 @@
 package com.example.datospersonales;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class validacion extends AppCompatActivity {
 
@@ -10,5 +11,27 @@ public class validacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validacion);
+
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String nombre = extras.getString("nombre");
+            String telefono = extras.getString("telefono");
+            String carrera = extras.getString("carrera");
+            String deporte = extras.getString("deporte");
+            String edad = extras.getString("edad");
+
+            EditText txt_nombre = findViewById(R.id.nombre);
+            EditText txt_telefono = findViewById(R.id.telefono);
+            EditText txt_carrera = findViewById(R.id.carrera);
+            EditText txt_deporte = findViewById(R.id.deporte);
+            EditText txt_edad = findViewById(R.id.edad);
+
+            txt_nombre.setText(nombre);
+            txt_telefono.setText(telefono);
+            txt_carrera.setText(carrera);
+            txt_deporte.setText(deporte);
+            txt_edad.setText(edad);
+        }
     }
 }
